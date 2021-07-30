bea947e7869c0076055538ccd642d5324ed295dc
Princess kelly <01kellyprincess@gmail.com>
2021-07-30 12:43:29 -0300

    programa terminado

--- programa_operadores.js
+++ programa_operadores.js
@@ -1,1 +1,40 @@
-const readline
+const readline = require('readline');
+
+const leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout 
+});
+
+const realizarCalculo = (num1, num2, operacao) => {
+    const primeiroNumero = parseInt(num1);
+    const segundoNumero = parseInt(num2);
+
+    switch (operacao) {
+        case '+':
+            return primeiroNumero + segundoNumero;
+        case '-' :
+            return primeiroNumero - segundoNumero;
+        case '*' :
+            return primeiroNumero * segundoNumero;
+        case '/' :
+            return primeiroNumero / segundoNumero;
+        default :
+            return 0;
+    }
+}
+
+leitor.question('Digite o primeiro número : \n', (primeiroNumero) => {
+    console.log({primeiroNumero });
+    
+    leitor.question('Digite o segundo número : \n', (segundoNumero) => {
+    console.log({segundoNumero});
+
+        leitor.question('Digite o operador : \n', (operacao) => {
+            const resultado = realizarCalculo(primeiroNumero, segundoNumero, operacao);
+
+            console.log({resultado});
+            leitor.close();
+
+        });
+    });
+});
